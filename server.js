ec5dac079569e5dac27c8ae6d175a777aeb10fab
photonrays <lthang052@gmail.com>
2024-02-18 21:06:25 +0700

    update cors

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ require('dotenv').config()
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 // Configure Cloudinary
 cloudinary.config({
